5487aba645b6774a2487c48fc55662bc6d5ed601
shaly <283199611@qq.com>
2019-03-21 22:45:25 +0800

    activeMQ：生产者设置session会话消息是否开启事务提交，如果true(开启)则必须进行session.commit才会提交，否则不会提交。如果false（不开启）则会在producer.send的时候自动提交到消息队列容器中

--- ActiveMQ/src/main/java/com/syf/study/p2p/ProducerTest.java
+++ ActiveMQ/src/main/java/com/syf/study/p2p/ProducerTest.java
@@ -1,5 +1,7 @@
 package com.syf.study.p2p;
 
+import java.util.Random;
+
 import javax.jms.Connection;
 import javax.jms.DeliveryMode;
 import javax.jms.JMSException;
@@ -16,7 +18,7 @@ public class ProducerTest {
 	private static String url="tcp://127.0.0.1:61616";
 	private static String myQueue="my_queue";
 	
-	public static void main(String[] args) throws JMSException {
+	public static void main(String[] args) throws JMSException, InterruptedException {
 		//1.创建连接工厂
 		ActiveMQConnectionFactory fa=new ActiveMQConnectionFactory(url);
 		//2.创建连接
@@ -24,9 +26,11 @@ public class ProducerTest {
 		connection.start();//启动连接
 		
 		//3.创建会话
-		//参数1:设置是否需要以实物方式提交
+		//参数1:设置是否需要以事务方式提交
+				//true:需要提交事务才会将session中的队列提交到消息队列容器中
+				//false:不需要提交事务，producer.send的时候会自动提交到队列容器中
 		//参数2：消息方式，默认采用自动签收(如果发送者设置手动签收，不会影响消费者的签收方式)
-		Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+		Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
 		//4.创建目标（主题）
 		Queue queue = session.createQueue(myQueue);
 
@@ -36,13 +40,15 @@ public class ProducerTest {
 		//PERSISTENT（持续化）
 		//NON_PERSISTENT（非持续化）
 		producer.setDeliveryMode(DeliveryMode.PERSISTENT);
-		for (int i = 10; i < 20; i++) {
+		for (int i = 101; i < 120; i++) {
 			//6.创建消息
 			TextMessage text=session.createTextMessage("主题内容："+i);
 			System.out.println(text);
+			//Thread.currentThread().sleep(10000);
 			//7.发送消息
 			producer.send(text);
 		}
+		session.commit();
 		//8.消息发送完毕
 		connection.close();
 	}
